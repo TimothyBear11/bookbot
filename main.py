649350cab5e2e9a47e75e09f53a8589ef307b246
TimothyBear11 <timothybear1183@pm.me>
2025-12-12 14:31:42 -0500

    updated main to be able to convert a book file to a string

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print("greetings boots")
+import os
+
+def get_book_text(bookLoc):
+  with open(bookLoc) as b:
+    book_contents = b.read()
+  return book_contents
+
+def main():
+  frank = os.path.expanduser('~/bookbot/bookbot/books/frankenstein.txt')  
+  text = get_book_text(frank)
+  print(text)
+
+main()  
+
